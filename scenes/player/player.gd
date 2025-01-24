31d293dfc1ca7100f23018a3e42f93842e35e37a
rocketCarlos <mscarlos.contact@gmail.com>
2025-01-25 00:13:29 +0100

    updated acceleration physics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 300.0
 
 const MAX_ACCEL: float = 1000.0
-const MAX_DEACCEL: float = 
+const MIN_ACCEL: float = 200.0
+var accel: float = MIN_ACCEL
+
+const MAX_DEACCEL: float = 1000.0
+const MIN_DEACCEL: float = 200.0
+var deaccel: float = MIN_DEACCEL
+# measures how slippery the surface is. 
+# The higher, the more inertia there is
+# between 0 and 1
+var slippery_factor: float = 1.0
+
+# between 0 and 100
+var water_level: float = 100.0
 
 func _ready() -> void:
 	update_acceleration()
@@ -15,19 +28,25 @@ func _physics_process(delta: float) -> void:
 	var direction = (mouse_position - global_position).normalized()
 	rotation = Vector2(0.0, 1.0).angle_to(direction)
 	
-	
 	# -----------------------------------------
 	# manage movement
 	# -----------------------------------------
 	if Input.is_action_pressed("accelerate"):
-		velocity -= velocity * slippery_factor
-		velocity += accel_per_second * direction * delta
+		# the acceleration force input by the player
+		var force = accel * direction * delta
+		# apply the force to velocity
+		velocity += force
+		# decide based on the slippery factor how important is the new force's
+		# direction in the final velocity. To do so, split the velocity's length
+		# into proportional parts for each direction (the original and the new)
+		velocity = (velocity.length()*slippery_factor) * velocity.normalized() + (velocity.length()*(1.0-slippery_factor)) * force.normalized()
+		
 		# Limit speed not to exceed MAX_SPEED
 		if velocity.length() >= MAX_SPEED:
 			velocity = velocity.normalized() * MAX_SPEED
 	else:
 		if velocity.length() > 0:
-			velocity -= velocity.normalized() * deaccel_per_second * delta
+			velocity -= velocity.normalized() * deaccel * delta
 			# Prevent overshooting and stop when speed is very low
 			if velocity.length() < 10.0:  # Threshold for stopping
 				velocity = Vector2(0, 0)
@@ -35,9 +54,6 @@ func _physics_process(delta: float) -> void:
 	# -------------------------
 	# manage water level
 	# -------------------------
-	print("water ", water_level)
-	print("accel ", accel_per_second)
-	print("deaccel ", deaccel_per_second)
 	water_level -= velocity.length() * 0.00025
 	
 	if water_level < 0:
@@ -48,8 +64,11 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 '''
-updates accel_per_second and deaccel_per_second depending on the water level
+updates accel and deaccel depending on the water level
 '''
 func update_acceleration() -> void:
-	#accel_per_second = MAX_ACCEL_PER_SECOND * (water_level / 100.0)
-	deaccel_per_second = MAX_DEACCEL_PER_SECOND * ((100.0-water_level) / 100.0)
+	# TODO: explore easing for values
+	slippery_factor = lerp(0.96, 1.0, water_level/100.0)
+	accel = lerp(MAX_ACCEL, MIN_ACCEL, water_level/100.0)
+	deaccel = lerp(MAX_DEACCEL, MIN_DEACCEL, water_level/100.0)
+	print(accel, " ", deaccel, " ", water_level)
