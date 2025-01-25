513298956f65cca370735ba96bdaaaaf23f2ff4f
rocketCarlos <mscarlos.contact@gmail.com>
2025-01-25 14:40:44 +0100

    added clock

---
+++ scenes/clock/clock.gd
@@ -0,0 +1,17 @@
+extends TextureProgressBar
+
+@onready var timer = $Timer
+@onready var minute = $Minute
+
+@export var total_time = 60
+var rotation_offset = 2.6
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.start(total_time)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	value = inverse_lerp(total_time, 0, timer.time_left) * 100.0
+	minute.rotation = lerp(0.0, 2*PI, value/100.0) + rotation_offset * PI / 180 
