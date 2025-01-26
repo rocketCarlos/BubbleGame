6028a5a3468351e4db03ab6ab5011d703bae7815
rocketCarlos <mscarlos.contact@gmail.com>
2025-01-26 12:55:21 +0100

    level building WIP

--- scenes/furniture/furniture.gd
+++ scenes/furniture/furniture.gd
@@ -7,4 +7,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	Globals.clean.emit([position])
+	Globals.clean.emit([global_position])
