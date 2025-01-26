90f2d10078bb74e1e95fbded911f97ca9b60263d
rocketCarlos <mscarlos.contact@gmail.com>
2025-01-26 10:39:45 +0100

    furniture practice

--- scenes/item/item.gd
+++ scenes/item/item.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 
+@onready var sprite = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sprite.scale = scale
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
