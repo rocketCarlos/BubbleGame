2be41cfcefc3caed5f3a98c34accaea65048e1ae
rocketCarlos <mscarlos.contact@gmail.com>
2025-01-26 16:35:42 +0100

    upgraded player animations

--- scenes/cutscene/cutscene.gd
+++ scenes/cutscene/cutscene.gd
@@ -3,12 +3,19 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation = "initial"
+	frame = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("accelerate"):
-		frame += 1
 		if frame == 3:
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "modulate", Color.TRANSPARENT, 1)
+			print("hello")
+			await tween.finished
+			print("ended")
 			Globals.start_game.emit()
+		frame += 1
+		
