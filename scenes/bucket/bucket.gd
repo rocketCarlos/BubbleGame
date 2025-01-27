d1a60d690bfc4fc3ad277ac3cfc4cae33e4748e6
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2025-01-27 21:52:46 +0100

    bucket clears dirt under it

--- scenes/bucket/bucket.gd
+++ scenes/bucket/bucket.gd
@@ -4,12 +4,12 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	Globals.clean.emit([global_position])
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
