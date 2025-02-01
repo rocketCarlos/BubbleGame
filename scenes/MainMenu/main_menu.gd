1602091cb0c5dcdb0c3d2f04dd4855dba1fd00d5
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2025-02-01 15:47:33 +0100

    Added credits

--- scenes/MainMenu/main_menu.gd
+++ scenes/MainMenu/main_menu.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var background = $Sprite2D
 @onready var start_button = $Sprite2D/Start
+@onready var credits_button = $Sprite2D/Credits
+@onready var credits = $Credits
 @onready var cutscene = $Cutscene
 
 # Called when the node enters the scene tree for the first time.
@@ -23,4 +25,15 @@ func _on_start_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	start_button.disabled = true
+	credits_button.disabled = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(credits, "position", Vector2(0.0, 0.0), 1).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	
+
+func _on_back_pressed() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(credits, "position", Vector2(-2200.0, 0.0), 1).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	await tween.finished
+	start_button.disabled = false
+	credits_button.disabled = false
